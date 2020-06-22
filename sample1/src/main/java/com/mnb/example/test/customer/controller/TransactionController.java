bbdae68da444c3c2c56b86d1633da432f81ec26e
manjunath <manjunathnbec@gmail.com>
2020-06-22 16:56:27 +0530

    customer txn info added

---
+++ sample1/src/main/java/com/mnb/example/test/customer/controller/TransactionController.java
@@ -0,0 +1,27 @@
+package com.mnb.example.test.customer.controller;
+
+import com.mnb.example.test.customer.model.TransactionInfo;
+import com.mnb.example.test.customer.service.CustomerTransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+
+    @Autowired
+    private CustomerTransactionService transactionService;
+
+    @GetMapping(value = "/transactions" , produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<TransactionInfo> getTransactions(){
+        return  transactionService.findAll();
+    }
+
+    @PostMapping(value = "/transaction/add" , produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public TransactionInfo addTransaction(@RequestBody TransactionInfo transactionInfo){
+        //TODO : Add assertions to validate the input
+        return transactionService.save(transactionInfo);
+    }
+
+}
