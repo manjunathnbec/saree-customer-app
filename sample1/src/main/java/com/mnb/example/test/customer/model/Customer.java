cd535fb59f8cc8a5b75b593d727731a8e86f05cd
manjunath <manjunathnbec@gmail.com>
2020-06-23 10:49:37 +0530

    adding lombok and logging

--- sample1/src/main/java/com/mnb/example/test/customer/model/Customer.java
+++ sample1/src/main/java/com/mnb/example/test/customer/model/Customer.java
@@ -6,10 +6,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Objects;
 
 @Entity
 @NoArgsConstructor
+@Setter
+@ToString
+@Getter@AllArgsConstructor
+@EqualsAndHashCode
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -26,65 +29,7 @@ public class Customer {
         this.balance = balance;
     }
 
-    public Long getCustomerID() {
-        return customerID;
-    }
-
-    public void setCustomerID(Long customerID) {
-        this.customerID = customerID;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Customer customer = (Customer) o;
-        return balance == customer.balance &&
-                Objects.equals(customerID, customer.customerID) &&
-                Objects.equals(name, customer.name) &&
-                Objects.equals(phone, customer.phone) &&
-                Objects.equals(address, customer.address);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(customerID, name, phone, address, balance);
-    }
-
-
-    @Override
+/*    @Override
     public String toString() {
         return "Customer{" +
                 "customerID=" + customerID +
@@ -93,5 +38,5 @@ public class Customer {
                 ", address='" + address + '\'' +
                 ", balance=" + balance +
                 '}';
-    }
+    }*/
 }
--- sample1/src/main/java/com/mnb/example/test/customer/service/CustomerTransactionService.java
+++ sample1/src/main/java/com/mnb/example/test/customer/service/CustomerTransactionService.java
@@ -4,15 +4,16 @@ import com.mnb.example.test.customer.model.Customer;
 import com.mnb.example.test.customer.model.TransactionInfo;
 import com.mnb.example.test.customer.repository.ICustomerRepository;
 import com.mnb.example.test.customer.repository.ITransactionRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.Transient;
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class CustomerTransactionService {
     @Autowired
@@ -26,16 +27,18 @@ public class CustomerTransactionService {
 
     @Transactional
     public TransactionInfo save(TransactionInfo transactionInfo) {
+
         Optional<Customer> customer = customerRepository.findById(transactionInfo.getCustomerId());
         if(customer.isPresent()){
             double balance = customer.get().getBalance();
             balance += transactionInfo.getTransactionAmount();
             balance -= transactionInfo.getAmountPaid();
             customer.get().setBalance(balance);
-            Customer updatedCustomer = customerRepository.save(customer.get());
-            //print the log
+            customerRepository.save(customer.get());
         }
         TransactionInfo txnInfo = transactionRepository.save(transactionInfo);
+        log.info("adding txn info {}", transactionInfo);
+        log.info("Updated customer info {}", customer.get());
         return txnInfo;
     }
 
